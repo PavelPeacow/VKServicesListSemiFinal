4c09d6825fcde06ebb659aa77d150f287480610f
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2023-02-18 11:54:26 +0500

    initial commit

---
+++ VKServicesList/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  VKServicesList
+//
+//  Created by Павел Кай on 18.02.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = VKServicesViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+
+}
+
